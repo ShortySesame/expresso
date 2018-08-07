baf009a03aef612c76ab2e5d5e040108cc892361
ShortySesame <32882415+ShortySesame@users.noreply.github.com>
2018-08-06 17:30:54 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const morgan = require('morgan');
 var cors = require('cors');
 
 
-app.use(cors());
+
 
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 const PORT = process.env.PORT || 4000;
 
-
+app.use(cors());
 app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
